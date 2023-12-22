83a24a5eb8364526f174614d4b7fc536cd650fdc
SchiavonNathan <Nathanmestrepk@gmail.com>
2023-12-22 08:45:58 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# baralho-em-GO
+ 
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+
+	//Inicialização de variavel
+	cards := newDeck()
+	//Chamando função para printar variavel(array no caso)
+	cards.print()
+}
